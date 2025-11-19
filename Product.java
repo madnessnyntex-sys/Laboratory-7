35a8c4acc4013db6f620aadb0ae180543dcf6a97
madnessnyntex-sys <madnessnyntex@gmail.com>
2025-11-19 11:44:41 +0800

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,1 +1,11 @@
+package com.example.laboratory7;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public class Product {
+    private Long id;
+    private String name;
+    private Double price;
+}
